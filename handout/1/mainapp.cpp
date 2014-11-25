6fd15a65e3b76e104ac9722ebd8c93f4a67445d8
Adorilson Bezerra <adorilson@gmail.com>
2014-11-25 20:42:20 -0300

    Improveiment the mainapp from first program
    It's was add some outputs and now is easier
    to change the execution params. Find the section
       /*
       * Set up the execution params
       */

--- handout/1/mainapp.cpp
+++ handout/1/mainapp.cpp
@@ -38,6 +38,11 @@
 
 #include "shuffler.hpp"        // Shuffler
 
+using std::cout ;
+using std::cerr ;
+using std::endl ;
+
+
 /**
  * \fn int main()
  *
@@ -62,12 +67,18 @@
  *
  * -------------------------------------------------------------------
  */
+ 
+void print(int* a, int l, int r){
+  return;
+  for(int i=l; i<=r; i++){
+    cerr << a[i] << " ";
+  }
+  cerr << endl;
+}
+ 
 int main()
 {
-  using std::cout ;
-  using std::cerr ;
-  using std::endl ;
-
+  
   /*
    * Instantiate the sorting objects.
    */
@@ -83,11 +94,19 @@ int main()
    * Set up the format for displaying the output numbers.
    */
   cout << std::scientific << std::setprecision( 16 ) ;
+  
+  /*
+   * Set up the execution params
+   */
+  int begin_power = 3;
+  int end_power = 20;
+  int times_sorting = 100;
 
   /*
-   * Run the method for arrays of size 2^i, where i = 3,...,20.
+   * Run the method for arrays of size 2^i
    */
-  cerr << "Executing the sorting method on 18 distinctly-sized arrays (100 times for each)..."
+  cerr << "Executing the sorting method on " << end_power - begin_power + 1
+        << "distinctly-sized arrays (" << times_sorting << ") times for each)..."
        << endl ;
 
   cerr << endl ;
@@ -97,22 +116,23 @@ int main()
 
   cerr << endl ;
 
-  int n = 8 ;
+  int n = 2;
+  n <<= begin_power-1;
 
-  for ( int i = 0 ; i < 18 ; i++ ) {
+  for ( int i = begin_power ; i <= end_power ; i++ ) {
     cerr << endl ;
     cerr << "-----------------------------------------------------" 
 	 << endl ;
     cerr << "Allocating memory for an array of size "
-         << n
+         << n << " (2^" << i << ")"
          << "..."
 	 << endl ;
 
     /*
      * Allocate memory for two arrays of n integers each.
      */
-    int* a = new int[ n ] ;
-    int* b = new int[ n ] ;
+     int* a = new  int[ n ] ;
+     int* b = new  int[ n ] ;
 
     cerr << "Filling in the array with integers from 1 to "
          << n
@@ -127,7 +147,7 @@ int main()
     }
 
     /*
-     * Execute the method 100 times, each of which takes in a possibly
+     * Execute the method some times, each of which takes in a possibly
      * distinct permutation of  the array.  This is not  ideal, as the
      * number of permutations of  the arrays grows exponentially as we
      * double the size of the array.   However, if we try to be "fair"
@@ -138,13 +158,13 @@ int main()
      * ideal).
      */
 
-    cerr << "Shuffling and sorting the array 100 times..."
+    cerr << "Shuffling and sorting the array " << times_sorting << " times..."
 	 << endl ;
 
     double mstime = 0 ;
     double qstime = 0 ;
 
-    for ( int j = 0 ; j < 100 ; j++ ) {
+    for ( int j = 0 ; j < times_sorting ; j++ ) {
       /*
        * Shuffle the  array (again). In principle,  any permutation of
        * the  array is  equally likely  to be  produced by  the method
@@ -209,15 +229,15 @@ int main()
        * execution.
        */
       for ( int j = 1 ; j < n ; j++ ) {
-	if ( b[ j ] < b[ j - 1 ] ) {
-	  cerr << "Your implementation of the quicksort algorithm is incorrect!" << endl ;
-	  return EXIT_FAILURE ;
-	}
+        if ( b[ j ] < b[ j - 1 ] ) {
+          cerr << endl << "Your implementation of the quicksort algorithm is incorrect!" << endl ;
+          return EXIT_FAILURE ;
+        }
       }
+      cerr << "Done sorting for array with " << n << " elements "
+        << "the " << j+1 << "th time" 
+        << endl ;
     }
-
-    cerr << "Done sorting..."
-         << endl ;
       
     /*
      * Write out array size and the average execution size.
@@ -236,15 +256,17 @@ int main()
      */
     n <<= 1 ;
 
+    cerr << "Release the memory allocated for the arrays " 
+         << endl ;
     /*
      * Release the memory allocated for the arrays "a" and "b".
      */
     if ( a != 0 ) {
-      delete a ;
+      delete []a ;
     }
 
     if ( b != 0 ) {
-      delete b ;
+      delete []b ;
     }
 
     cerr << "-----------------------------------------------------" 
